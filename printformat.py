c7132121e91522ed5522d09ff7593aeed75b8509
valentinov <valentinvecsernik@gmail.com>
2023-08-12 16:34:04 +0200

    Create printformat.py

---
+++ printformat.py
@@ -0,0 +1,12 @@
+city = "Budapest"
+job = "DevOps engineer"
+
+print("I live in", city)
+
+print("I live in {}".format(city))
+
+print("I live in {} and work as a {}".format(city, job))
+
+print("I live in", city, "and work as a", job)
+
+print(f"I live in {city} and work as a {job}")
